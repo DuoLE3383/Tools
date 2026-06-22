42a5906e5d5f57961b23483e26dcf2c14399f119
DuoLe3383 <duole.hcm@gmail.com>
2026-06-23 06:44:53 +0700

    Update MrrRigCard.jsx

--- src/components/mrr/MrrRigCard.jsx
+++ src/components/mrr/MrrRigCard.jsx
@@ -9,7 +9,7 @@ import {
   getStatusClass,
   getRoiColor,
   getNiceHashPriceValue,
-} from "../core/mrrUtils.js";
+} from "../../core/mrrUtils.js";
 import {
   HASHRATE_SUFFIXES,
   getAlgoDisplayName,
